a6e79ef67aa241e4a5b01a67d33174f86384e4f7
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2023-05-17 13:24:18 -0500

    deploy: update application.properties

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -jar /usr/local/lib/app.jar --spring.datasource.url=jdbc:postgresql://agripure.postgres.database.azure.com:5432/db_agripure --spring.datasource.username=team_agridevelopers \
+--spring.datasource.password=tucOvQR3d8lB8b7vp0ZS4kEg31HqmHFJ
